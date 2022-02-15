c8ab9c32b917a9b00875d0abb91207e095d41637
StJohn Piano <stjohnpiano@gmail.com>
2022-02-15 13:45:42 +0100

    Expand Test. Create test for changing PIN.

--- src/application/SolidiMobileApp/components/Main/components/MainPanel/components/Test/Test.js
+++ src/application/SolidiMobileApp/components/Main/components/MainPanel/components/Test/Test.js
@@ -1,15 +1,51 @@
 // React imports
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { StandardButton } from 'src/components/atomic';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { mainPanelStates } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
 
 
 let Test = () => {
 
+  let appState = useContext(AppStateContext);
+
+  //appState.loadPIN();
+
   return (
-    <Text>Test</Text>
+    <View style={styles.panelContainer}>
+      <Text>Status: </Text>
+      <StandardButton title='Log out' style={styleButton}
+        onPress={ () => { appState.logOut(); } }
+      />
+      <StandardButton title='Change PIN' style={styleButton}
+        onPress={ () => { appState.choosePIN(); } }
+      />
+
+    </View>
   )
 
 }
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingTop: scaledHeight(80),
+    paddingHorizontal: scaledWidth(15),
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+
+let styleButton = StyleSheet.create({
+  marginTop: 10,
+});
 
 
 export default Test;
