9e403282985f3f85bef8fd40b22aabaf2a648c09
StJohn Piano <stjohnpiano@gmail.com>
2022-02-24 17:47:20 +0100

    import trustpilot.png

--- src/images/index.js
+++ src/images/index.js
@@ -3,9 +3,12 @@
 // Reason: Webpack can only bundle resources that it can identify at bundle time.
 
 import solidi from './solidi.png';
+import trustpilot from './trustpilot.png';
+
 
 let ImageLookup = {
   solidi,
+  trustpilot,
 }
 
 export default ImageLookup;
