d3a5cf5b947e2037df0ed116b8c792f724824618
StJohn Piano <stjohnpiano@gmail.com>
2022-02-25 14:08:47 +0100

    InsufficientBalance

---
+++ src/application/SolidiMobileApp/components/MainPanel/components/InsufficientBalance/InsufficientBalance.js
@@ -0,0 +1,166 @@
+// React imports
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { assetsInfo, mainPanelStates, colors } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, ImageButton } from 'src/components/atomic';
+import misc from 'src/util/misc';
+
+
+
+
+let InsufficientBalance = () => {
+
+  let appState = useContext(AppStateContext);
+
+  /* We can arrive at this component due to:
+  - trying to submit an buy order and pay with our balance, and we don't have enough.
+  - trying to submit a sell order for an amount that we don't have.
+  - trying to make a withdrawal that is too large.
+  */
+  let pageName = appState.pageName;
+  let permittedPageNames = 'buy sell withdraw'.split(' ');
+  misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'InsufficientBalance');
+
+  // Testing:
+  _.assign(appState.panels.buy, {volumeQA: '10000', assetQA: 'GBP', volumeBA: '0.05', assetBA: 'BTC'});
+  _.assign(appState.apiData, {
+    balance: {
+      BTC: {"balance": "0.00000000"},
+      GBP: {"balance": "8900.00000000"},
+    },
+  });
+
+  // Load order details.
+  ({volumeQA, volumeBA, assetQA, assetBA} = appState.panels.buy);
+
+  // We assume that prior to loading this component, we have retrieved the balances from the API. So, here we can just load them directly.
+  let balanceQA = appState.apiData.balance[assetQA].balance;
+  let dp = assetsInfo[assetQA].decimalPlaces;
+  let diffString = Big(volumeQA).minus(Big(balanceQA)).toFixed(dp);
+  let balanceString = Big(balanceQA).toFixed(dp);
+  let volumeString = Big(volumeQA).toFixed(dp);
+  let orderDetails = `Buy ${volumeBA} ${assetsInfo[assetBA].displayString} for ${volumeQA} ${assetsInfo[assetQA].displayString}.`;
+
+  let payDirectly = () => {
+    appState.changeState('ChooseHowToPay', 'direct_payment');
+  }
+
+  let makeDeposit = () => {
+    // Todo: Not implemented yet.
+    appState.changeState('Receive', assetQA);
+  }
+
+  return (
+    <View style={styles.panelContainer}>
+    <View style={styles.panelSubContainer}>
+
+      <View style={[styles.heading, styles.heading1]}>
+        <Text style={styles.headingText}>Insufficient balance</Text>
+      </View>
+
+      <Text>Order details: {orderDetails}</Text>
+
+      <View style={styles.infoSection}>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>{`\u2022  `} Your Solidi balance: {balanceString} {assetQA}</Text>
+        </View>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>{`\u2022  `} The required amount: {volumeString} {assetQA}</Text>
+        </View>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>{`\u2022  `} The difference: {diffString} {assetQA}</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.infoSection}>
+        <View style={styles.infoItem}>
+          <Text>Sorry!</Text>
+        </View>
+      </View>
+
+      <View style={styles.infoSection}>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>Option 1:</Text>
+          <Text>{'\n'}Go back and pay for the order using your online banking.</Text>
+        </View>
+
+        <View style={styles.button}>
+          <StandardButton title="Go back" onPress={ payDirectly } />
+        </View>
+
+      </View>
+
+      <View style={styles.infoSection}>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>Option 2:</Text>
+          <Text>{'\n'}Increase your {assetQA} balance by making a deposit.</Text>
+        </View>
+
+        <View style={styles.button}>
+          <StandardButton title="Make a deposit" onPress={ makeDeposit } />
+        </View>
+
+      </View>
+
+
+
+    </View>
+    </View>
+  )
+
+}
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingHorizontal: scaledWidth(15),
+    paddingVertical: scaledHeight(5),
+    width: '100%',
+    height: '100%',
+  },
+  panelSubContainer: {
+    paddingTop: scaledHeight(10),
+    paddingHorizontal: scaledWidth(30),
+  },
+  heading: {
+    alignItems: 'center',
+  },
+  heading1: {
+    marginTop: scaledHeight(10),
+    marginBottom: scaledHeight(20),
+  },
+  headingText: {
+    fontSize: normaliseFont(20),
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  infoSection: {
+    paddingTop: scaledHeight(20),
+    alignItems: 'flex-start',
+  },
+  infoItem: {
+    marginBottom: scaledHeight(5),
+  },
+  button: {
+    marginTop: scaledHeight(10),
+  },
+});
+
+
+export default InsufficientBalance;
