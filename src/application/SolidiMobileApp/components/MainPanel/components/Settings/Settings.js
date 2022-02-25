083d93e85524d11308211d502f7908867565832a
StJohn Piano <stjohnpiano@gmail.com>
2022-02-25 19:09:07 +0100

    Expand Settings basic code

--- src/application/SolidiMobileApp/components/MainPanel/components/Settings/Settings.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Settings/Settings.js
@@ -1,15 +1,82 @@
 // React imports
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { assetsInfo, mainPanelStates, colors } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, ImageButton } from 'src/components/atomic';
+import misc from 'src/util/misc';
+
+
 
 
 let Settings = () => {
 
+  let appState = useContext(AppStateContext);
+
   return (
-    <Text>Settings</Text>
+    <View style={styles.panelContainer}>
+    <View style={styles.panelSubContainer}>
+
+      <View style={[styles.heading, styles.heading1]}>
+        <Text style={styles.headingText}>Settings</Text>
+      </View>
+
+      <Text style={styles.bold}>{'\n'} {`\u2022  `} [some text]</Text>
+
+      <View style={styles.infoSection}>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>{`\u2022  `} [some details]</Text>
+        </View>
+
+      </View>
+
+    </View>
+    </View>
   )
 
 }
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingHorizontal: scaledWidth(15),
+    paddingVertical: scaledHeight(5),
+    width: '100%',
+    height: '100%',
+  },
+  panelSubContainer: {
+    paddingTop: scaledHeight(10),
+    paddingHorizontal: scaledWidth(30),
+  },
+  heading: {
+    alignItems: 'center',
+  },
+  heading1: {
+    marginTop: scaledHeight(10),
+  },
+  headingText: {
+    fontSize: normaliseFont(20),
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  infoSection: {
+    paddingTop: scaledHeight(20),
+    alignItems: 'flex-start',
+  },
+  infoItem: {
+    marginBottom: scaledHeight(5),
+  },
+});
 
 
 export default Settings;
