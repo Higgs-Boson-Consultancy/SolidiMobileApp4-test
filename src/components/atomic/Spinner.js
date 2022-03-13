477d24d02bdf3bc328b9ca5c85a1e8f13bab4546
StJohn Piano <stjohnpiano@gmail.com>
2022-03-13 15:53:16 +0100

    Spinner (to show during loading)

---
+++ src/components/atomic/Spinner.js
@@ -0,0 +1,19 @@
+// React imports
+import React, { useContext, useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, StyleSheet, View } from 'react-native';
+
+
+let Spinner = () => {
+  return (
+    <View style={StyleSheet.create({
+      height: '80%',
+      //borderWidth: 1,
+      justifyContent: 'center',
+    })}>
+      <ActivityIndicator color={'blue'} size={'large'} />
+    </View>
+  );
+}
+
+
+export default Spinner;
--- src/components/atomic/index.js
+++ src/components/atomic/index.js
@@ -2,9 +2,11 @@
 import Button from './Button/Button';
 import StandardButton from './StandardButton';
 import ImageButton from './ImageButton/ImageButton';
+import Spinner from './Spinner';
 
 export {
   Button,
   StandardButton,
   ImageButton,
+  Spinner,
 };
