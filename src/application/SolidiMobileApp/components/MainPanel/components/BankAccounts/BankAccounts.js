e2b45de13372b4ab32ce0c7094c1ec332110c84b
StJohn Piano <stjohnpiano@gmail.com>
2022-03-24 17:46:27 +0100

    BankAccounts.js

---
+++ src/application/SolidiMobileApp/components/MainPanel/components/BankAccounts/BankAccounts.js
@@ -0,0 +1,115 @@
+// React imports
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { mainPanelStates, colors } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, ImageButton, Spinner } from 'src/components/atomic';
+import misc from 'src/util/misc';
+
+
+/* Notes
+
+
+*/
+
+
+
+
+let BankAccounts = () => {
+
+  let appState = useContext(AppStateContext);
+  let [renderCount, triggerRender] = useState(0);
+  let firstRender = misc.useFirstRender();
+  let stateChangeID = appState.stateChangeID;
+
+  let pageName = appState.pageName;
+  let permittedPageNames = 'default'.split(' ');
+  misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'BankAccounts');
+
+
+  let account1 = appState.user.info.defaultAccount.GBP;
+  let accountAsset = 'GBP';
+
+
+  // Initial setup.
+  useEffect( () => {
+    setup();
+  }, []); // Pass empty array so that this only runs once on mount.
+
+
+  let setup = async () => {
+    await appState.loadUserInfo();
+    if (appState.stateChangeIDHasChanged(stateChangeID)) return;
+    triggerRender(renderCount+1);
+  }
+
+
+  return (
+    <View style={styles.panelContainer}>
+    <View style={styles.panelSubContainer}>
+
+      <View style={[styles.heading, styles.heading1]}>
+        <Text style={styles.headingText}>Bank Account</Text>
+      </View>
+
+      <View style={styles.bankAccount}>
+        <Text style={styles.bankAccountText}>Bank Account: {'\n'}</Text>
+        <Text style={styles.bankAccountText}>{`\u2022  `} {account1.accountName}</Text>
+        <Text style={styles.bankAccountText}>{`\u2022  `} Sort Code: {account1.sortCode}</Text>
+        <Text style={styles.bankAccountText}>{`\u2022  `} Account Number: {account1.accountNumber}</Text>
+        <Text style={styles.bankAccountText}>{`\u2022  `} Currency: {appState.getAssetInfo(accountAsset).displayString}</Text>
+      </View>
+
+    </View>
+    </View>
+  )
+
+}
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingHorizontal: scaledWidth(15),
+    paddingVertical: scaledHeight(5),
+    width: '100%',
+    height: '100%',
+  },
+  panelSubContainer: {
+    paddingTop: scaledHeight(10),
+    paddingHorizontal: scaledWidth(30),
+  },
+  heading: {
+    alignItems: 'center',
+  },
+  heading1: {
+    marginTop: scaledHeight(10),
+  },
+  headingText: {
+    fontSize: normaliseFont(20),
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  bankAccount: {
+    marginTop: scaledHeight(60),
+    padding: scaledWidth(20),
+    paddingTop: scaledWidth(15),
+    borderWidth: 1,
+  },
+  bankAccountText: {
+    marginTop: scaledHeight(5),
+    fontWeight: 'bold',
+    fontSize: normaliseFont(16),
+  },
+});
+
+
+export default BankAccounts;
