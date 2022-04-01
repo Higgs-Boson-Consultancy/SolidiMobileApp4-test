90afa95778d4419fc2e02e629eb62392432b6cd2
StJohn Piano <stjohnpiano@gmail.com>
2022-04-01 16:36:11 +0200

    Move import

--- src/application/SolidiMobileApp/components/MainPanel/components/Buy/Buy.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Buy/Buy.js
@@ -10,8 +10,8 @@ import Big from 'big.js';
 // Internal imports
 import AppStateContext from 'src/application/data';
 import { mainPanelStates, colors } from 'src/constants';
-import { Button, StandardButton, Spinner } from 'src/components/atomic';
 import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, Spinner } from 'src/components/atomic';
 import misc from 'src/util/misc';
 
 // Logger
