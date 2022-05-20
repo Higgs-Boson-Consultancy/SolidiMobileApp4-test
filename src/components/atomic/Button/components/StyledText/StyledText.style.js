c5aa1a024408d3ae1d246c54d4aa6aab05dc27a4
StJohn Piano <stjohnpiano@gmail.com>
2022-05-20 15:20:55 +0200

    Restructure StyledText.style.js (set color to be constant, among other things)

--- src/components/atomic/Button/components/StyledText/StyledText.style.js
+++ src/components/atomic/Button/components/StyledText/StyledText.style.js
@@ -1,41 +1,58 @@
-
+// React imports
 import React from 'react';
 import { Platform, StyleSheet, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-const { style } = StyleSheet.create({
+// Other imports
+import _ from 'lodash';
+
+// Internal imports
+import { colors } from 'src/constants';
+
+// Logger
+import logger from 'src/util/logger';
+let logger2 = logger.extend('Button');
+let {deb, dj, log, lj} = logger.getShortcuts(logger2);
+
+
+
+
+let { style } = StyleSheet.create({
   style: {
     textAlign: 'center',
     margin: 4,
     padding: 4,
+    color: colors.buttonText,
     ...Platform.select({
       ios: {
-        color: '#007AFF',
         fontSize: 16,
       },
       android: {
-        color: 'white',
         fontWeight: '500',
       },
     }),
   },
 });
 
-const StyledText = ({ disabled, styles, ...rest }) => {
-  const combinedStyles = StyleSheet.flatten([style, styles]);
+
+let StyledText = ({ disabled, styles, ...rest }) => {
+  let combinedStyles = StyleSheet.flatten([style, styles]);
 
   return (
     <Text style={combinedStyles} {...rest} />
   );
 };
 
+
 StyledText.defaultProps = {
   styles: {},
 };
+
 
 StyledText.propTypes = {
   disabled: PropTypes.bool.isRequired,
   styles: PropTypes.object,
 };
+
 
 export default StyledText;
