b0dc3d5ccde361fc03c2e62ea265465f278d7c29
StJohn Piano <stjohnpiano@gmail.com>
2022-05-24 20:43:38 +0200

    Fix

--- src/application/SolidiMobileApp/components/MainPanel/components/Error/Error.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Error/Error.js
@@ -47,7 +47,7 @@ let Error = () => {
 
   let setup = async () => {
     try {
-      // await appState.loadInitialStuffAboutUser();
+      // await appState.generalSetup();
       if (appState.stateChangeIDHasChanged(stateChangeID)) return;
       triggerRender(renderCount+1);
     } catch(err) {
