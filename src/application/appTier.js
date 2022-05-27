280a7ae2f0c989efe9506c01267c1cfc32682420
StJohn Piano <stjohnpiano@gmail.com>
2022-05-27 16:49:48 +0200

    Move appTier.js

---
+++ src/application/appTier.js
@@ -0,0 +1,7 @@
+
+// Options: 'dev', 'stag', 'prod'.
+// Note: The log level depends on this setting.
+
+export const appTier = 'stag';
+
+export default appTier;
--- src/constants/appTier.js
+++
@@ -1,7 +0,0 @@
-
-// Options: 'dev', 'prod'.
-// Note: Log levels change depending on this setting.
-
-export const appTier = 'dev';
-
-export default appTier;
