05f878dc2a65a249d80454ef073265249af6e54c
StJohn Piano <stjohnpiano@gmail.com>
2022-06-01 14:00:19 +0200

    SupportTools.js

---
+++ src/application/SolidiMobileApp/components/MainPanel/components/SupportTools/SupportTools.js
@@ -0,0 +1,137 @@
+// React imports
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Text, StyleSheet, View, ScrollView } from 'react-native';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { mainPanelStates, colors } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, ImageButton, Spinner } from 'src/components/atomic';
+import misc from 'src/util/misc';
+
+// Logger
+import logger from 'src/util/logger';
+let logger2 = logger.extend('SupportTools');
+let {deb, dj, log, lj} = logger.getShortcuts(logger2);
+
+
+/* Notes
+
+Here, we have buttons to provide access to any tool that is:
+- rarely used
+- complex / advanced
+- experimental
+
+*/
+
+
+
+
+let SupportTools = () => {
+
+  let appState = useContext(AppStateContext);
+  let [renderCount, triggerRender] = useState(0);
+  let firstRender = misc.useFirstRender();
+  let stateChangeID = appState.stateChangeID;
+
+  let pageName = appState.pageName;
+  let permittedPageNames = 'default'.split(' ');
+  misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'SupportTools');
+
+
+  // Initial setup.
+  useEffect( () => {
+    setup();
+  }, []); // Pass empty array so that this only runs once on mount.
+
+
+  let setup = async () => {
+    try {
+      await appState.generalSetup();
+      if (appState.stateChangeIDHasChanged(stateChangeID)) return;
+      triggerRender(renderCount+1);
+    } catch(err) {
+      let msg = `SupportTools.setup: Error = ${err}`;
+      console.log(msg);
+    }
+  }
+
+
+  return (
+    <View style={styles.panelContainer}>
+    <View style={styles.panelSubContainer}>
+
+      <View style={[styles.heading, styles.heading1]}>
+        <Text style={styles.headingText}>Support Tools</Text>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={true} style={styles.mainScrollView}>
+
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Register without auto-login'
+          onPress={ () => { appState.changeState('Register', 'nonAuto'); } }
+        />
+      </View>
+
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Delete all local data and log out'
+          onPress={ async () => {
+            // Delete the PIN (from both the app memory and from the Keychain).
+            await appState.deletePIN(deleteFromKeychain=true);
+            // Log out: This will delete everything else.
+            await appState.logout();
+            appState.changeState('Login');
+          } }
+        />
+      </View>
+
+      </ScrollView>
+
+    </View>
+    </View>
+  )
+
+}
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingHorizontal: scaledWidth(15),
+    paddingVertical: scaledHeight(5),
+    width: '100%',
+    height: '100%',
+  },
+  panelSubContainer: {
+    paddingTop: scaledHeight(10),
+    paddingHorizontal: scaledWidth(30),
+    height: '100%',
+    //borderWidth: 1, // testing
+  },
+  heading: {
+    alignItems: 'center',
+  },
+  heading1: {
+    marginTop: scaledHeight(10),
+    marginBottom: scaledHeight(40),
+  },
+  headingText: {
+    fontSize: normaliseFont(20),
+    fontWeight: 'bold',
+  },
+  mainScrollView: {
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  buttonWrapper: {
+    marginVertical: scaledHeight(10),
+    width: '100%',
+  },
+});
+
+
+export default SupportTools;
