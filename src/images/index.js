99c1632f71adf16a75a50c663958cf0bff8487d3
StJohn Piano <stjohnpiano@gmail.com>
2022-06-01 18:21:31 +0200

    Import new logo

--- src/images/index.js
+++ src/images/index.js
@@ -2,7 +2,7 @@
 // - The function "require" can't accept a variable.
 // Reason: Webpack can only bundle resources that it can identify at bundle time.
 
-import solidi from './solidi.png';
+import solidi_logo_landscape_black_1924x493 from './solidi_logo_landscape_black_1924x493.png';
 import trustpilot from './trustpilot.png';
 import asset_icons from './asset_icons';
 
@@ -10,7 +10,7 @@ let { GBP, BTC, ETH } = asset_icons;
 
 
 let ImageLookup = {
-  solidi,
+  solidi_logo_landscape_black_1924x493,
   trustpilot,
   GBP,
   BTC,
